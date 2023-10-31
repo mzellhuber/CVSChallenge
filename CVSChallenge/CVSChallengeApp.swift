080e266130c4c9ef713d1d950feb2cdd2af0e528
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2023-10-31 15:28:27 -0600

    Initial Commit

---
+++ CVSChallenge/CVSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSChallengeApp.swift
+//  CVSChallenge
+//
+//  Created by Melissa Zellhuber on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CVSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
